726457268544ab3b07ec2058718d5946c8981e8f
buriburi <sagarshayr999@gmail.com>
2021-05-09 08:02:05 +0530

    concrete view classes padha which is the best method to create api and perform CRUD operation

--- apiProject/apiApp/urls.py
+++ apiProject/apiApp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('create',views.StudentCreate.as_view()),
     path('update/<int:pk>',views.StudentUpdate.as_view()),
     path('delete/<int:pk>',views.StudentDelete.as_view()),
+    path('listcreate',views.StudentListCreate.as_view()),
+    path('retrieveupdate/<int:pk>',views.StudentRetrieveUpdate.as_view()),
+    path('retrievedestroy/<int:pk>',views.StudentRetrieveDestroy.as_view()),
+    path('retrievedestroyupdate/<int:pk>',views.StudentRetrieveDestroyUpdate.as_view()),
 ]
--- apiProject/apiApp/views.py
+++ apiProject/apiApp/views.py
@@ -1,34 +1,62 @@
 from apiApp import models,serializers
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin,CreateModelMixin,UpdateModelMixin,RetrieveModelMixin,DestroyModelMixin
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView,ListCreateAPIView,RetrieveUpdateAPIView,RetrieveDestroyAPIView,RetrieveUpdateDestroyAPIView
 
-class StudentList(GenericAPIView,ListModelMixin):
+class StudentList(ListAPIView):
     queryset=models.Student.objects.all()
     serializer_class=serializers.StudentSerializer
-    def get(self,request,*args,**kwargs):
-        return self.list(request,*args,**kwargs)
 
-class StudentCreate(GenericAPIView,CreateModelMixin):
+class StudentCreate(CreateAPIView):
     queryset=models.Student.objects.all()
     serializer_class=serializers.StudentSerializer
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs)
 
-class StudentRetrieve(GenericAPIView,RetrieveModelMixin):
+class StudentRetrieve(RetrieveAPIView):
     queryset=models.Student.objects.all()
     serializer_class=serializers.StudentSerializer
-    def get(self,request,*args,**kwargs):
-        return self.retrieve(request,*args,**kwargs)
 
-class StudentUpdate(GenericAPIView,UpdateModelMixin):
+class StudentUpdate(UpdateAPIView):
     queryset=models.Student.objects.all()
     serializer_class=serializers.StudentSerializer
-    def get(self,request,*args,**kwargs):
-        return self.update(request,*args,**kwargs)
 
-class StudentDelete(GenericAPIView,DestroyModelMixin):
+class StudentDelete(DestroyAPIView):
     queryset=models.Student.objects.all()
     serializer_class=serializers.StudentSerializer
-    def get(self,request,*args,**kwargs):
-        return self.destroy(request,*args,**kwargs)
+
+class StudentListCreate(ListCreateAPIView):
+    queryset=models.Student.objects.all()
+    serializer_class=serializers.StudentSerializer
+
+class StudentRetrieveUpdate(RetrieveUpdateAPIView):
+    queryset=models.Student.objects.all()
+    serializer_class=serializers.StudentSerializer
+
+class StudentRetrieveDestroy(RetrieveDestroyAPIView):
+    queryset=models.Student.objects.all()
+    serializer_class=serializers.StudentSerializer
+
+
+class StudentRetrieveDestroyUpdate(RetrieveUpdateDestroyAPIView):
+    queryset=models.Student.objects.all()
+    serializer_class=serializers.StudentSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
